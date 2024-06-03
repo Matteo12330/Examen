846589aa01a7ff3ee2a272145f311914120d52a0
laboratorio <laboratorio@UPEAULA-31720>
2024-06-03 07:32:28 -0500

    Agregar archivos de proyecto.

---
+++ prueba/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace prueba
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnRechargeClicked(object sender, EventArgs e)
+        {
+            // Aquí implementa la lógica para realizar la recarga
+            ConfirmationLabel.Text = $"Recarga de {AmountPicker.SelectedItem} realizada con éxito.";
+        }
+    }
+}
